e2ea511430a9a64def4a942bf7aa0c2989b8281f
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-02-28 14:30:44 +0000

    adding new link

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,6 +83,9 @@ const Home: NextPage = () => {
             {/* <Button onClick={() => handleButtonClick("/Profiles/Jimathy")} className="ml-2 flex-shrink">
               Slim Jim&apos;s Page
             </Button> */}
+            <Button onClick={() => handleButtonClick("/DROSInWorks")} className="ml-2 flex-shrink">
+              Future DROS
+            </Button>
             <Button onClick={() => handleButtonClick("/DialogHover")} className="ml-2 flex-shrink px-8 py-2 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500">
               Hovering Page
             </Button>
