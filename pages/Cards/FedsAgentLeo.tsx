c5cd4bd3b0939e1f4bb5da6bec23f937865998b8
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-03-02 22:07:57 +0000

    adding fedsagentleo

--- pages/Cards/FedsAgentLeo.tsx
+++ pages/Cards/FedsAgentLeo.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import React, { useEffect, useState } from 'react';
 import { useRouter } from "next/router";
-
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../../components/ui/navbar-menu";
   import {
     Card,
     CardContent,
@@ -10,45 +10,37 @@ import { useRouter } from "next/router";
     CardTitle,
   } from "../../components/ui/card"
   
-  function FederalAgent ({ className }: { className?: string }) {
+  function FedsAgentLeo ({ className }: { className?: string }) {
+  const [activeDialog, setActiveDialog] = useState<string | null>(null);
+
   const router = useRouter();
+  const handleOpenDialog = (dialogId: string) => setActiveDialog(dialogId);
+  const handleCloseDialog = () => setActiveDialog(null);
 
     return (
         <div className="flex flex-col items-center justify-center w-full" >
             <Card className="flex flex-col w-full">
                 <CardHeader>
-                <CardTitle>Federal Agent DROS Info</CardTitle>
+                <CardTitle>Federal Agent Info</CardTitle>
                 <CardDescription>
-                <span className="text-amber-500">Before Moving Forward</span>, Ask For A Business Card With Agency | Department Letterhead | Seal<br/>
-                And <span className="text-orange-500">PRINT</span> THE Federal Doc Worksheet In The Info Section Below
+                <span className="text-amber-500">DROS Exemption Support Can Be Found Under &quot;DROS Exemptions Guide&quot; Menu</span>
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
                 <div className="max-w-[425px]">
-                <h2>DROS FSC Exemption Fields:</h2>
-                 <ul className="list-none pl-4">
-                  <p>FSC Exemption Code</p>
-                    <ul className="list-none pl-4">
-                      <li><p>(X32 - Peace Officer - Federal - Active |</p> 
-                      <p>X33 - Peace Officer - Federal - Honorably Retired)</p>
-                      <p><span className="blue-text text-darken-3">ONLY USED FOR FEDERAL AGENTS</span></p></li>
-                      </ul>
-                    <p>Non-Roster Exemption</p>
-                      <ul className="list-none pl-4">
-                        <li>(AGENCY THEY ARE EMPLOYED WITH)
-                        <br/>You <span className="red-text text-darken-2">CANNOT</span> Copy Federal ID&apos;s,</li>
-                      </ul>
-                      </ul>
+                <h2>Federal Agents Are Listed As <span className="text-amber-500">Group 1</span></h2>
+                <p>You <span className="text-red-500">CANNOT</span> Copy Federal ID&apos;s,</p>
+                <p>Make Sure You Print The <span className="text-amber-500">Federal Doc Worksheet</span></p>
                     <p>Here Are The Required Docs:</p>
                       <ul className="list-none pl-4">
-                        <li>A Scan | Copy Of CA DL | CA ID</li>
+                        <li><HoveredLink onClick={() => handleOpenDialog('fedsIdVerification')} href="#">Federal ID Verification</HoveredLink></li>
                         <li>A Business Card With Agency | Department Letterhead | Seal</li>
                       </ul>
                        <br/>
                     <p>Federal Agents Are <span className="text-red-500">NOT</span> 30 Day Exempt</p>
                       <hr className="my-4" />
                       <span className="text-amber-500">Before Moving Forward</span>, Ask For A Business Card With Agency | Department Letterhead | Seal<br/>
-                      And <span className="text-orange-500">PRINT</span> THE Federal Doc Worksheet In The Info Section Below
+                      And <span className="text-orange-500">PRINT</span> The Federal Doc Worksheet In The Info Section Below
                       <hr/>
                       <br/>If The Transaction Is For A <span className="text-blue-500">Blue Label</span> Firearm,<br/>
                       Don&apos;t Forget To Fill Out The <span className="text-red-500">Blue Label</span> Form & Include It In The Pending Packet
@@ -61,4 +53,4 @@ import { useRouter } from "next/router";
         </div>
     )
   }
-  export default FederalAgent;
+  export default FedsAgentLeo;
