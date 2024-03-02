951758735b1e2ea7805267a55eb5c28fde359005
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-03-02 22:17:10 +0000

    tried updating

--- pages/Cards/FedsAgentLeo.tsx
+++ pages/Cards/FedsAgentLeo.tsx
@@ -11,11 +11,8 @@ import { HoveredLink, Menu, MenuItem, ProductItem } from "../../components/ui/na
   } from "../../components/ui/card"
   
   function FedsAgentLeo ({ className }: { className?: string }) {
-  const [activeDialog, setActiveDialog] = useState<string | null>(null);
 
   const router = useRouter();
-  const handleOpenDialog = (dialogId: string) => setActiveDialog(dialogId);
-  const handleCloseDialog = () => setActiveDialog(null);
 
     return (
         <div className="flex flex-col items-center justify-center w-full" >
@@ -31,16 +28,10 @@ import { HoveredLink, Menu, MenuItem, ProductItem } from "../../components/ui/na
                 <h2>Federal Agents Are Listed As <span className="text-amber-500">Group 1</span></h2>
                 <p>You <span className="text-red-500">CANNOT</span> Copy Federal ID&apos;s,</p>
                 <p>Make Sure You Print The <span className="text-amber-500">Federal Doc Worksheet</span></p>
-                    <p>Here Are The Required Docs:</p>
+                    <p>You Can Print The Federal Doc Worksheet Here:</p>
                       <ul className="list-none pl-4">
-                        <li><HoveredLink href="https://drive.google.com/file/d/1ruAqN1-iRgUtTeSfiVmYZHOiosLzFjmE/view?usp=drive_link"><span className="text-orange-500">Federal Doc Worksheet</span></HoveredLink></li>
-                        <li>A Business Card With Agency | Department Letterhead | Seal</li>
+                        <li><a href="https://drive.google.com/file/d/1ruAqN1-iRgUtTeSfiVmYZHOiosLzFjmE/view?usp=drive_link"><span className="text-orange-500">Federal Doc Worksheet</span></a></li>
                       </ul>
-                       <br/>
-                    <p>Federal Agents Are <span className="text-red-500">NOT</span> 30 Day Exempt</p>
-                      <hr className="my-4" />
-                      <span className="text-amber-500">Before Moving Forward</span>, Ask For A Business Card With Agency | Department Letterhead | Seal<br/>
-                      And <span className="text-orange-500">PRINT</span> The Federal Doc Worksheet In The Info Section Below
                       <hr/>
                       <br/>If The Transaction Is For A <span className="text-blue-500">Blue Label</span> Firearm,<br/>
                       Don&apos;t Forget To Fill Out The <span className="text-red-500">Blue Label</span> Form & Include It In The Pending Packet
